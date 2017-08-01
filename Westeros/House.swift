a104dc141bc86ee168c506b39d0ad3e049bdfb28
Carlos <thehandyowl.io@gmail.com>
2017-08-01 19:34:47 +0200

    Falta 1 archivo

--- Westeros/House.swift
+++ Westeros/House.swift
@@ -22,7 +22,8 @@ final class House{
     let sigil   : Sigil
     let words   : Words
     let wikiURL : URL
-    private var _members : Members
+    //private var _members : Members
+    var _members : Members
     
     init(name: String, sigil: Sigil, words: Words, url: URL){
         (self.name, self.sigil, self.words, self.wikiURL) = (name, sigil, words, url)
