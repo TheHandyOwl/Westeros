36eadef3dbfaa02f52b62c386b5ca3c66d8db192
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2017-08-02 17:40:59 +0200

    Ejercicio corregido con la práctica facilitada

--- Westeros/AppDelegate.swift
+++ Westeros/AppDelegate.swift
@@ -16,20 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        // Crear Window
+        // Model
+        let houses = Repository.local.houses
+        
+        // Controllers
+        let dataSource = DataSources.houseDataSource(model: houses)
+        let housesVC = ArrayTableViewController(dataSource: dataSource,
+                                                  delegate: HousesDelegate(),
+                                                     title: "Westeros",
+                                                     style: .plain).wrappedInNavigation()
+
+        // Window
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = UIColor.cyan
-        
-        // Creamos los modelos
-        let houses = Repository.local.houses
-        let mainDelegate = Delegates.housesDelegate(model: houses)
-        
-        // Creamos los controladores
-        let dataSource = DataSources.houseDataSource(model: houses)
-        let housesVC = ArrayTableViewController(dataSource: dataSource, delegate: mainDelegate, title: "Westeros", style: .plain).wrappedInNavigation()
-
-        // Asignamos el RootVC
         window?.rootViewController = housesVC
 
         return true
